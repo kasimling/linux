cc6afaad867c00f22b00ae909ba20cf1d23cfa4b
Ilho Lee <ilho215.lee@samsung.com>
2009-01-20 10:00:36 +0900

    A bug fixed in gpio-bank-g.h

--- arch/arm/plat-s3c64xx/include/plat/gpio-bank-g.h
+++ arch/arm/plat-s3c64xx/include/plat/gpio-bank-g.h
@@ -37,7 +37,7 @@
 #define S3C64XX_GPG4_MMC0_DATA2		(0x02 << 16)
 #define S3C64XX_GPG4_EINT_G5_4		(0x07 << 16)
 
-#define S3C64XX_GPG4_MMC0_DATA2		(0x02 << 20)
+#define S3C64XX_GPG5_MMC0_DATA3		(0x02 << 20)
 #define S3C64XX_GPG5_EINT_G5_5		(0x07 << 20)
 
 #define S3C64XX_GPG6_MMC0_CD		(0x02 << 24)
