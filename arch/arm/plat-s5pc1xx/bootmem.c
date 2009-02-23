b82ae703939bcbf774d00192f2c5ac609b8f381d
Jinsung Yang <jsgood.yang@samsung.com>
2009-02-23 14:26:18 +0900

    modified:   arch/arm/configs/smdkc100mtd_defconfig 	modified:   arch/arm/configs/smdkc100nfs_defconfig 	modified:   arch/arm/plat-s5pc1xx/bootmem.c

--- arch/arm/plat-s5pc1xx/bootmem.c
+++ arch/arm/plat-s5pc1xx/bootmem.c
@@ -15,7 +15,126 @@
 #include <linux/bootmem.h>
 #include <linux/swap.h>
 #include <asm/setup.h>
+#include <asm/io.h>
 #include <mach/memory.h>
+
+#include "plat/media.h"
+
+static struct s3c_media_device s3c_mdevs[] = {
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_FIMC
+	{
+		.id = S3C_MDEV_FIMC,
+		.name = "fimc",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_FIMC * SZ_1K,
+		.paddr = 0,
+	},
+#endif
+
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_POST
+	{
+		.id = S3C_MDEV_POST,
+		.name = "pp",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_POST * SZ_1K,
+		.paddr = 0,
+	},
+#endif
+
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_TV
+	{
+		.id = S3C_MDEV_TV,
+		.name = "tv",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_TV * SZ_1K,
+		.paddr = 0,
+	},
+#endif
+
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_MFC
+	{
+		.id = S3C_MDEV_MFC,
+		.name = "mfc",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_MFC * SZ_1K,
+		.paddr = 0,
+	},
+#endif
+
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_JPEG
+	{
+		.id = S3C_MDEV_JPEG,
+		.name = "jpeg",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_JPEG * SZ_1K,
+		.paddr = 0,
+	},
+#endif
+
+#ifdef CONFIG_VIDEO_SAMSUNG_MEMSIZE_CMM
+	{
+		.id = S3C_MDEV_CMM,
+		.name = "cmm",
+		.memsize = CONFIG_VIDEO_SAMSUNG_MEMSIZE_CMM * SZ_1K,
+		.paddr = 0,
+	}
+#endif
+};
+
+static struct s3c_media_device *s3c_get_media_device(int dev_id)
+{
+	struct s3c_media_device *mdev = NULL;
+	int i, found;
+
+	if (dev_id < 0 || dev_id >= S3C_MDEV_MAX)
+		return NULL;
+
+	i = 0;
+	found = 0;
+	while (!found && (i < S3C_MDEV_MAX)) {
+		mdev = &s3c_mdevs[i];
+		if (mdev->id == dev_id)
+			found = 1;
+		else
+			i++;
+	}
+
+	if (!found)
+		mdev = NULL;
+
+	return mdev;
+}
+
+dma_addr_t s3c_get_media_memory(int dev_id)
+{
+	struct s3c_media_device *mdev;
+
+	mdev = s3c_get_media_device(dev_id);
+	if (!mdev){
+		printk(KERN_ERR "invalid media device\n");
+		return 0;
+	}
+
+	if (!mdev->paddr) {
+		printk(KERN_ERR "no memory for %s\n", mdev->name);
+		return 0;
+	}
+
+	return mdev->paddr;
+}
+
+void s5pc1xx_reserve_bootmem(void)
+{
+	struct s3c_media_device *mdev;
+	int i;
+
+	for(i = 0; i < sizeof(s3c_mdevs) / sizeof(s3c_mdevs[0]); i++) {
+		mdev = &s3c_mdevs[i];
+		if (mdev->memsize > 0) {
+			mdev->paddr = virt_to_phys(alloc_bootmem_low(mdev->memsize));
+			printk(KERN_INFO \
+				"s5pc1xx: %lu bytes SDRAM reserved "
+				"for %s at 0x%08x\n",
+				(unsigned long) mdev->memsize, \
+				mdev->name, mdev->paddr);
+		}
+	}
+}
 
 /* FIXME: temporary implementation to avoid compile error */
 int dma_needs_bounce(struct device *dev, dma_addr_t addr, size_t size)
@@ -23,30 +142,3 @@ int dma_needs_bounce(struct device *dev, dma_addr_t addr, size_t size)
 	return 0;
 }
 
-void s5pc1xx_reserve_bootmem(void)
-{
-	struct bootmem_data *bdata;
-	unsigned long sdram_start, sdram_size;
-	unsigned long reserved;
-
-	bdata = NODE_DATA(0)->bdata;
-	sdram_start = bdata->node_min_pfn << PAGE_SHIFT;
-	sdram_size = (bdata->node_low_pfn << PAGE_SHIFT) - sdram_start;
-	reserved = 0;
-
-	sdram_start &= PAGE_MASK;
-	
-	/* add here for devices' bootmem size */
-#ifdef CONFIG_VIDEO_FIMC_STATIC_MEMORY
-	reserved += CONFIG_VIDEO_FIMC_STATIC_MEMSIZE * SZ_1K;
-#endif
-
-	if (reserved > 0) {
-		reserve_bootmem(sdram_start, \
-				PAGE_ALIGN(sdram_size - reserved), BOOTMEM_DEFAULT);
-
-		printk(KERN_INFO \
-			"s3c64xx: %lu bytes SDRAM reserved at 0x%08x\n", reserved, \
-			(unsigned int) (sdram_start + sdram_size - reserved));
-	}
-}
