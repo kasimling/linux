9277d5013afb88835b9e4b3d8f7e0e93d9b4b4b8
Alex Ling <kasimling@gmail.com>
2013-01-13 12:09:47 +0800

    s5pv210: Add minimal support for MINI210
    Signed-off-by: Alex Ling <kasimling@gmail.com>

---
+++ arch/arm/mach-s5pv210/mach-mini210.c
@@ -0,0 +1,106 @@
+/* Copyright (c) 2013 Alex Ling <kasimling@gmail.com>
+ *
+ * based on linux/arch/arm/mach-s5pv210/mach-torbreck.c
+ *
+ * Copyright (c) 2010 aESOP Community
+ *		http://www.aesop.or.kr/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/init.h>
+#include <linux/serial_core.h>
+
+#include <asm/hardware/vic.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/setup.h>
+#include <asm/mach-types.h>
+
+#include <mach/map.h>
+#include <mach/regs-clock.h>
+
+#include <plat/regs-serial.h>
+#include <plat/devs.h>
+#include <plat/cpu.h>
+#include <plat/s5p-time.h>
+
+#include "common.h"
+
+/* Following are default values for UCON, ULCON and UFCON UART registers */
+#define MINI210_UCON_DEFAULT	(S3C2410_UCON_TXILEVEL |	\
+				 S3C2410_UCON_RXILEVEL |	\
+				 S3C2410_UCON_TXIRQMODE |	\
+				 S3C2410_UCON_RXIRQMODE |	\
+				 S3C2410_UCON_RXFIFO_TOI |	\
+				 S3C2443_UCON_RXERR_IRQEN)
+
+#define MINI210_ULCON_DEFAULT	S3C2410_LCON_CS8
+
+#define MINI210_UFCON_DEFAULT	(S3C2410_UFCON_FIFOMODE |	\
+				 S5PV210_UFCON_TXTRIG4 |	\
+				 S5PV210_UFCON_RXTRIG4)
+
+static struct s3c2410_uartcfg mini210_uartcfgs[] __initdata = {
+	[0] = {
+		.hwport		= 0,
+		.flags		= 0,
+		.ucon		= MINI210_UCON_DEFAULT,
+		.ulcon		= MINI210_ULCON_DEFAULT,
+		.ufcon		= MINI210_UFCON_DEFAULT,
+	},
+	[1] = {
+		.hwport		= 1,
+		.flags		= 0,
+		.ucon		= MINI210_UCON_DEFAULT,
+		.ulcon		= MINI210_ULCON_DEFAULT,
+		.ufcon		= MINI210_UFCON_DEFAULT,
+	},
+	[2] = {
+		.hwport		= 2,
+		.flags		= 0,
+		.ucon		= MINI210_UCON_DEFAULT,
+		.ulcon		= MINI210_ULCON_DEFAULT,
+		.ufcon		= MINI210_UFCON_DEFAULT,
+	},
+	[3] = {
+		.hwport		= 3,
+		.flags		= 0,
+		.ucon		= MINI210_UCON_DEFAULT,
+		.ulcon		= MINI210_ULCON_DEFAULT,
+		.ufcon		= MINI210_UFCON_DEFAULT,
+	},
+};
+
+static struct platform_device *mini210_devices[] __initdata = {
+	&s3c_device_rtc,
+	&s3c_device_wdt,
+};
+
+static void __init mini210_map_io(void)
+{
+	s5pv210_init_io(NULL, 0);
+	s3c24xx_init_clocks(24000000);
+	s3c24xx_init_uarts(mini210_uartcfgs, ARRAY_SIZE(mini210_uartcfgs));
+	s5p_set_timer_source(S5P_PWM2, S5P_PWM4);
+}
+
+static void __init mini210_machine_init(void)
+{
+	platform_add_devices(mini210_devices, ARRAY_SIZE(mini210_devices));
+}
+
+MACHINE_START(MINI210, "MINI210")
+	/* Maintainer: Alex Ling <kasimling@gmail.com> */
+	.atag_offset	= 0x100,
+	.init_irq	= s5pv210_init_irq,
+	.handle_irq	= vic_handle_irq,
+	.map_io		= mini210_map_io,
+	.init_machine	= mini210_machine_init,
+	.timer		= &s5p_timer,
+	.restart	= s5pv210_restart,
+MACHINE_END
